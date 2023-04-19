0e519b931900dc1447b8d6b36d38065a7a2e10fc
VictorGutierrez <vagutierrezv@uqvirtual.edu.co>
2023-04-19 10:34:54 -0500

    prueba

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styleUrls: ['./incrementador.component.css']
 })
-export class IncrementadorComponent implements OnInit {
+export class IncrementadorComponent {
 
-  constructor() { }
+@Input() progreso: number = 40;
 
-  ngOnInit(): void {
-  }
+cambiarValor (valor: number) {
+   if (this.progreso >= 100 && valor >=0) {
+      return this.progreso = 100;
+   }
+
+   if (this.progreso <=0  && valor < 0) {
+    return this.progreso = 0;
+ }
+}
+
+
+
 
 }
